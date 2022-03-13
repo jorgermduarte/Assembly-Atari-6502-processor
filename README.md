9af13cf306a8c79f64301be38802cad63ddbb416
Jorge Duarte <joka867@hotmail.com>
2022-03-13 20:32:23 +0000

    feat: added cleanmemory.asm file.

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
-# Assembly-Atari-6502-processor
+# Assembly-Atari-6502-processor-game
 A simple game made in assembly for the Atari 2600 based on the 6502 processor.
+
+# Assembling the code
+
+**Currently using dasm assembler to test the assembly code created.** 
+>  - The dasm assembler as support for several 8-bit microprocessors including MOS 6502 & 6507, Motorola 6803, 68705 & 68HC11, Hitachi HD6303 (extended Motorola 6801), and Fairchild F8.
+
+
+> - https://dasm-assembler.github.io/
+
+
+# Testing the game
+
+Right now i'm using the stella emulator to test the game that i'm creating.
+> - https://stella-emu.github.io/
+
---
+++ utils/cleanmemory/cleanmemory.asm
@@ -0,0 +1,30 @@
+    processor 6502
+
+    seg code
+    org $F000 ; define the code origin at $F000
+
+Start:
+    sei       ; disable interrupts
+    cld       ; disable the bcd decimal math mode
+    ldx #$FF  ; loads the x register with #$FF
+    txs       ; transfer the x register to the (S)tack pointer
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+; Clear the page zero region ($00 to $FF)
+; Meaning the entire RAM and also the entire TIA Registers
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+    lda #0         ; A = 0
+    ldx #$FF       ; X = #$FF
+
+MemoryLoop:
+    sta $0,X       ; store the value of A inside the memory address $0 + X
+    dex            ; X--
+    bne MemoryLoop ; branch if not equal to zero to memory loop (Loop until x is equal to zero) (z-flag is set)
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+; Fill the ROM size to exactly 4KB
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+    org $FFFC      ; Set the origin to the end of the cartige
+    .word Start    ; Reset vector at $FFFC ( where the program starts)
+    .word Start    ; Interrupt vector at $FFFE (unused in the VCS)
+
+
+
